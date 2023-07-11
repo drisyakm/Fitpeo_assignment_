ef6aeeba6d30d5dc55614f56a8433f33a67b6071
drisya-bleems <89334102+drisya-bleems@users.noreply.github.com>
2023-07-11 13:28:11 +0300

    Final modifications

--- app/src/main/java/com/example/fitpeo_assignment/view/DetailActivity.kt
+++ app/src/main/java/com/example/fitpeo_assignment/view/DetailActivity.kt
@@ -1,7 +1,8 @@
 package com.example.fitpeo_assignment.view
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.example.fitpeo_assignment.R
 import com.example.fitpeo_assignment.constants.AppConstants
@@ -12,12 +13,23 @@ class DetailActivity : AppCompatActivity() {
     private var selectedAlbum :Album?=null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         val binding:ActivityDetailBinding = DataBindingUtil.setContentView(this, R.layout.activity_detail)
 
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.title = resources.getString(R.string.details_screen_title)
         selectedAlbum = intent.getParcelableExtra(AppConstants.SELECTED_ALBUM)
         binding.album = selectedAlbum
 
+
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val id = item.itemId
+
+        if (id == android.R.id.home) {
+            finish()
+        }
+        return super.onOptionsItemSelected(item)
 
     }
 }
